ba88f8dec99b4f4b252b5e05a9f4e4ad7aac0b4c
João V. Veras <joaovitorzv@outlook.com>
2022-05-31 22:59:16 -0300

    (programming): lecture 3

--- programming/scheme/lecture-3.rkt
+++ programming/scheme/lecture-3.rkt
@@ -11,13 +11,26 @@
   (if (> a b)
     0
     (+ (* a a a) (sumsquare (+ a 1) b)) ))
+
     
-;(define square 1)
-;(define circle 3.1415)
+(define square 1)
+(define circle 3.1415)
 
 ;(area square 2)
 ;(area circle 3)
 
 ;(sumsquare 3 5)
 ;(sumcube 3 5)
-(recursion 4 10)
+
+(define (sumsquared a)
+  (* a a))
+
+(define (sum fn a b)
+  (if (> a b)
+    0
+    (+ (fn a) (sum fn (+ a 1) b)) ))
+
+;(sum sumsquared 3 5)
+
+(member? 'e 'get)
+((lambda (x) x * x) 6)
